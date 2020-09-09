d77e2b0dae53161473e540e3bdf273ce98231b7c
Max <max120199@gmail.com>
2020-09-09 16:54:27 +0300

    May be?

--- lab2/cuda_image.h
+++ lab2/cuda_image.h
@@ -299,9 +299,9 @@ public:
         );
 
         // use clamp optimisation for limit exits
+        g_text.normalized = false;
         g_text.addressMode[0] = cudaAddressModeClamp;
         g_text.addressMode[1] = cudaAddressModeClamp;
-        g_text.normalized = false;
 
         uint32_t bloks_x = _height / MAX_X;
         uint32_t bloks_y = _widht / MAX_Y;
