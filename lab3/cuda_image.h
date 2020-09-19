6b0153ee14a4f121e9fef50e08008ae1ff7e7b40
Max <max120199@gmail.com>
2020-09-20 00:24:51 +0300

    gg

--- lab3/cuda_image.h
+++ lab3/cuda_image.h
@@ -644,7 +644,7 @@ private:
         double A32 = matr[0]*matr[5] - matr[2]*matr[3];
         double A33 = matr[0]*matr[4] - matr[1]*matr[3];
 
-        double D = A11 * matr[1] - A12 * matr[2] + A13 * matr[3];
+        double D = A11 * matr[0] - A12 * matr[1] + A13 * matr[2];
 
         matr[0] = A11 / D;
         matr[1] = A12 / D;
