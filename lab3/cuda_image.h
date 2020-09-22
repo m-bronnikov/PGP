299f9ef1f60be46d7318b6d44726a295c318d738
Max <max120199@gmail.com>
2020-09-22 16:07:58 +0300

    Lab 3 logs:

--- lab3/cuda_image.h
+++ lab3/cuda_image.h
@@ -15,10 +15,10 @@
 using namespace std;
 
 // max threads is 512 in block => sqrt(512) is dim
-#define MAX_X 4
-#define MAX_Y 4
-#define BLOCKS_X 32
-#define BLOCKS_Y 32
+#define MAX_X 32
+#define MAX_Y 32
+#define BLOCKS_X 16
+#define BLOCKS_Y 16
 
 #define RED(x) (x)&255
 #define GREEN(x) ((x) >> 8)&255
