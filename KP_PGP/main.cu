23d77595ad27b2b398876561ab962974b1ecfa2b
Maksim Bronnikov <max120199@gmail.com>
2021-04-02 20:47:01 +0300

    Update main

--- KP_PGP/main.cu
+++ KP_PGP/main.cu
@@ -94,111 +94,112 @@ int main(int argc, char** argv){
     }
 
     // Read properties of rendering scene
-    uint32_t frames;
-
-    string path_modifier;
-
-    uint32_t width, height;
-    float view_angle;
-
-    float rc_0, zc_0, fc_0, Ac_r, Ac_z, wc_r, wc_z, wc_f, pc_r, pc_z;
-    float rn_0, zn_0, fn_0, An_r, An_z, wn_r, wn_z, wn_f, pn_r, pn_z;
-
-    float_3 center, color;
-    float radius;
-    float reflection, refraction;
-    uint32_t line_lights;
-
-    float_3 floor_A, floor_B, floor_C, floor_D;
-    string texture_path;
-    float_3 texture_color;
-    float texture_refl;
-
-    uint32_t light_num;
-
-    uint32_t recursion_depth, sqrt_per_pixel;
-
-    // read:
-    // 1.
-    is >> frames;
-
-    // 2.
-    is >> path_modifier;
-
-    // 3.
-    is >> width >> height;
-    is >> view_angle;
-
-    // 4.
-    is >> rc_0 >> zc_0 >> fc_0 >> Ac_r >> Ac_z >> wc_r >> wc_z >> wc_f >> pc_r >> pc_z;
-    is >> rn_0 >> zn_0 >> fn_0 >> An_r >> An_z >> wn_r >> wn_z >> wn_f >> pn_r >> pn_z;
-    // create main objects
-    FileWriter writter(width, height, path_modifier, is_gpu);
-    Camera camera(
-        frames, proc_rank, num_of_procs, view_angle, 
-        rc_0, zc_0, fc_0, Ac_r, Ac_z, wc_r, wc_z, wc_f, pc_r, pc_z,
-        rn_0, zn_0, fn_0, An_r, An_z, wn_r, wn_z, wn_f, pn_r, pn_z
-    );
     {
-    Scene scene(camera, writter, MpiLogger(cout, proc_rank, num_of_procs), is_gpu);
+        uint32_t frames;
 
-    // 5.
-    // Tetraeder:
-    is >> center.x >> center.y >> center.z;
-    is >> color.x >> color.y >> color.z;
-    is >> radius;
-    is >> reflection >> refraction;
-    is >> line_lights; 
-    material fig_glass = {color, 0.5, reflection, refraction}; // set diffusion as 0.5 by default
-    scene.add_figure(Tetraeder(radius, center, fig_glass, line_lights));
+        string path_modifier;
 
-    // Octaeder:
-    is >> center.x >> center.y >> center.z;
-    is >> color.x >> color.y >> color.z;
-    is >> radius;
-    is >> reflection >> refraction;
-    is >> line_lights; 
-    fig_glass = {color, 0.5, reflection, refraction}; // set diffusion as 0.5 by default
-    scene.add_figure(Octaeder(radius, center, fig_glass, line_lights));
+        uint32_t width, height;
+        float view_angle;
 
-    // Dodecaedr:
-    is >> center.x >> center.y >> center.z;
-    is >> color.x >> color.y >> color.z;
-    is >> radius;
-    is >> reflection >> refraction;
-    is >> line_lights; 
-    fig_glass = {color, 0.5, reflection, refraction}; // set diffusion as 0.5 by default
-    scene.add_figure(Dodecaedr(radius, center, fig_glass, line_lights));
+        float rc_0, zc_0, fc_0, Ac_r, Ac_z, wc_r, wc_z, wc_f, pc_r, pc_z;
+        float rn_0, zn_0, fn_0, An_r, An_z, wn_r, wn_z, wn_f, pn_r, pn_z;
 
-    // 6.
-    is >> floor_A.x >> floor_A.y >> floor_A.z;
-    is >> floor_B.x >> floor_B.y >> floor_B.z;
-    is >> floor_C.x >> floor_C.y >> floor_C.z;
-    is >> floor_D.x >> floor_D.y >> floor_D.z;
-    is >> texture_path;
-    is >> texture_color.x >> texture_color.y >> texture_color.z;
-    is >> texture_refl;
-    material texture_mat = {texture_color, 0.5, texture_refl, 0.0}; // setd diffusion as 1.0
+        float_3 center, color;
+        float radius;
+        float reflection, refraction;
+        uint32_t line_lights;
 
-    // 7.
-    is >> light_num;
-    for(uint8_t i = 0; i < light_num; ++i){
-        float_3 light_coord, light_color;
-        is >> light_coord.x >> light_coord.y >> light_coord.z;
-        is >> light_color.x >> light_color.y >> light_color.z;
+        float_3 floor_A, floor_B, floor_C, floor_D;
+        string texture_path;
+        float_3 texture_color;
+        float texture_refl;
 
-        scene.add_light({light_coord, light_color, 1.0}); // set power as 1 by default
-    }
+        uint32_t light_num;
 
-    // 8.
-    is >> recursion_depth >> sqrt_per_pixel;
+        uint32_t recursion_depth, sqrt_per_pixel;
 
-    // set window before render:
-    scene.set_window(width, height, sqrt_per_pixel);
-    scene.set_floor(texture_path, floor_A, floor_B, floor_C, floor_D, texture_mat);
+        // read:
+        // 1.
+        is >> frames;
 
-    // launch render ;)
-    scene.render_scene(recursion_depth);
+        // 2.
+        is >> path_modifier;
+
+        // 3.
+        is >> width >> height;
+        is >> view_angle;
+
+        // 4.
+        is >> rc_0 >> zc_0 >> fc_0 >> Ac_r >> Ac_z >> wc_r >> wc_z >> wc_f >> pc_r >> pc_z;
+        is >> rn_0 >> zn_0 >> fn_0 >> An_r >> An_z >> wn_r >> wn_z >> wn_f >> pn_r >> pn_z;
+        // create main objects
+        FileWriter writter(width, height, path_modifier, is_gpu);
+        Camera camera(
+            frames, proc_rank, num_of_procs, view_angle, 
+            rc_0, zc_0, fc_0, Ac_r, Ac_z, wc_r, wc_z, wc_f, pc_r, pc_z,
+            rn_0, zn_0, fn_0, An_r, An_z, wn_r, wn_z, wn_f, pn_r, pn_z
+        );
+        
+        Scene scene(camera, writter, MpiLogger(cout, proc_rank, num_of_procs), is_gpu);
+
+        // 5.
+        // Tetraeder:
+        is >> center.x >> center.y >> center.z;
+        is >> color.x >> color.y >> color.z;
+        is >> radius;
+        is >> reflection >> refraction;
+        is >> line_lights; 
+        material fig_glass = {color, 0.5, reflection, refraction}; // set diffusion as 0.5 by default
+        scene.add_figure(Tetraeder(radius, center, fig_glass, line_lights));
+
+        // Octaeder:
+        is >> center.x >> center.y >> center.z;
+        is >> color.x >> color.y >> color.z;
+        is >> radius;
+        is >> reflection >> refraction;
+        is >> line_lights; 
+        fig_glass = {color, 0.5, reflection, refraction}; // set diffusion as 0.5 by default
+        scene.add_figure(Octaeder(radius, center, fig_glass, line_lights));
+
+        // Dodecaedr:
+        is >> center.x >> center.y >> center.z;
+        is >> color.x >> color.y >> color.z;
+        is >> radius;
+        is >> reflection >> refraction;
+        is >> line_lights; 
+        fig_glass = {color, 0.5, reflection, refraction}; // set diffusion as 0.5 by default
+        scene.add_figure(Dodecaedr(radius, center, fig_glass, line_lights));
+
+        // 6.
+        is >> floor_A.x >> floor_A.y >> floor_A.z;
+        is >> floor_B.x >> floor_B.y >> floor_B.z;
+        is >> floor_C.x >> floor_C.y >> floor_C.z;
+        is >> floor_D.x >> floor_D.y >> floor_D.z;
+        is >> texture_path;
+        is >> texture_color.x >> texture_color.y >> texture_color.z;
+        is >> texture_refl;
+        material texture_mat = {texture_color, 0.5, texture_refl, 0.0}; // setd diffusion as 1.0
+
+        // 7.
+        is >> light_num;
+        for(uint8_t i = 0; i < light_num; ++i){
+            float_3 light_coord, light_color;
+            is >> light_coord.x >> light_coord.y >> light_coord.z;
+            is >> light_color.x >> light_color.y >> light_color.z;
+
+            scene.add_light({light_coord, light_color, 1.0}); // set power as 1 by default
+        }
+
+        // 8.
+        is >> recursion_depth >> sqrt_per_pixel;
+
+        // set window before render:
+        scene.set_window(width, height, sqrt_per_pixel);
+        scene.set_floor(texture_path, floor_A, floor_B, floor_C, floor_D, texture_mat);
+
+        // launch render ;)
+        scene.render_scene(recursion_depth);
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
